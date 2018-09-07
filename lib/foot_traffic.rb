d96dde1b0c3c95ae8bb04fcec464e69cf6f2ce4e
vinaybongale1010 <vinaybongale1010@gmail.com>
2018-09-08 00:35:58 +0530

    implements the logic for foot traffic analysis

--- lib/foot_traffic.rb
+++ lib/foot_traffic.rb
@@ -1,8 +1,11 @@
 class FootTraffic
+
+  attr_reader :visitor_identifier, :room_index, :visitor_status, :event_time_stamp
+
   @@filepath = nil
+
   def self.filepath=(path=nil)
     @@filepath = File.join(APP_ROOT, path)
-    puts @@filepath
   end
   
   def self.file_usable?
@@ -11,4 +14,60 @@ class FootTraffic
     return false unless File.readable?(@@filepath)
     return true
   end
+
+  def self.fetch_foot_traffics
+    number_of_foot_traffics = nil
+    foot_traffics = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      number_of_foot_traffics = file.gets.chomp
+      file.each_line do |line|
+        foot_traffics << FootTraffic.new.import_line(line.chomp)
+      end
+      file.close
+    end
+    return foot_traffics, number_of_foot_traffics
+  end
+
+  ## searches for rooms available and sorts it in ascending order
+  def self.rooms_available(foot_traffics)
+    rooms = []
+    rooms = foot_traffics.each_with_object([]){|item,rooms| rooms << item.room_index.to_i}.uniq.sort
+    rooms
+  end
+
+  ## calulates the average time spent in each room by visitors
+  def self.average_time_spent_by_visitors_in_each_room(rooms, foot_traffics)
+    rooms.each do |room_number|
+      time_spent = 0
+      #evaluates entry records in the room
+      room_entry_records = entry_records_in_room(room_number, foot_traffics)
+      #evaluates outgoing records in the room
+      room_outgoing_records = outgoing_records_in_room(room_number, foot_traffics)
+      #evaluates the time spent in each room by visitor(s)
+      room_outgoing_records.each do |room_outgoing_record|
+        room_entry_record = room_entry_records.select{|e| e.visitor_identifier == room_outgoing_record.visitor_identifier && e.room_index == room_outgoing_record.room_index && e.visitor_status == "I"}.first
+        time_spent += room_outgoing_record.event_time_stamp.to_i - room_entry_record.event_time_stamp.to_i + 1
+      end
+      puts "Room #{room_number}, #{time_spent/room_outgoing_records.count} minute average visit, #{room_outgoing_records.count} visitors total" if room_outgoing_records.count > 1
+      puts "Room #{room_number}, #{time_spent/room_outgoing_records.count} minute average visit, #{room_outgoing_records.count} visitor total" if room_outgoing_records.count == 1            
+    end
+  end
+
+  ## calculates entry records in the room
+  def self.entry_records_in_room(room_number, foot_traffics)
+    foot_traffics.each_with_object([]){|item,room_entry_record| room_entry_record << item if item.room_index.to_i == room_number && item.visitor_status == "I"}
+  end     
+
+  ## calculates outgoing records in the room
+  def self.outgoing_records_in_room(room_number, foot_traffics)
+    foot_traffics.each_with_object([]){|item,room_outgoing_record| room_outgoing_record << item if item.room_index.to_i == room_number && item.visitor_status == "O"}
+  end
+  
+  def import_line(line)
+    line_array = line.split(" ")
+    @visitor_identifier, @room_index, @visitor_status, @event_time_stamp = line_array unless line_array.empty?
+    return self
+  end
+
 end
--- lib/guide.rb
+++ lib/guide.rb
@@ -1,11 +1,12 @@
 require 'foot_traffic'
+
 class Guide
   def initialize(path=nil)
-    # locate the foot traffic text file at path
+    ## locate the foot traffic text file at path
     FootTraffic.filepath = path
     if FootTraffic.file_usable?
       puts "\nFound foot traffic file."
-    # exit if file not found
+    ## exit if file not found
     else
       puts "\nFoot traffic Log file not found!!." 
       puts "Please create a foot traffic text file 'sample_input.txt' with required data inside the projects root directory!!.\n\n"
@@ -15,13 +16,18 @@ class Guide
 
   def launch!
     introduction
-    # foot traffic logic goes here
+
+    ## foot traffic logic goes here
+    foot_traffics, number_of_foot_traffics = FootTraffic.fetch_foot_traffics
+    rooms = FootTraffic.rooms_available(foot_traffics)
+    FootTraffic.average_time_spent_by_visitors_in_each_room(rooms, foot_traffics)
+
 		conclusion
   end
   
   
   def introduction
-    puts "\n\n<<< Welcome to the Food Traffic Analysis >>>\n"
+    puts "\n<<< Welcome to the Food Traffic Analysis >>>\n\n"
   end
 
 	def conclusion
